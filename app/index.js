6f80c44d63816ad9d4a97ba50867b82c587c4b68
wangpin34 <270280522@qq.com>
2018-08-14 13:35:15 +0800

    Feature: mobile adaptation

--- app/index.js
+++ app/index.js
@@ -1,27 +1,56 @@
 import marked from 'marked'
 import hljs from 'highlight.js'
+import $ from "jquery";
+
 import 'index.css'
-//require('../node_modules/highlight.js/styles/default.css')
-require('../node_modules/highlight.js/styles/dark.css')
+//require('../node_modules/highlight.js/styles/xcode.css')
+//require('../node_modules/highlight.js/styles/github.css')
+
 require('../node_modules/github-markdown-css/github-markdown.css')
+require('../node_modules/highlight.js/styles/github.css')
 
 hljs.initHighlightingOnLoad()
 
-document.addEventListener('DOMContentLoaded', () => {
+$(document).ready(() => {
 
-  let inputBox = document.getElementById('input-box')
-  let outputBox = document.getElementById('output-box')
-
-  inputBox.addEventListener('keyup', (e) => {
-    let input = inputBox.innerText
-    //console.info(`input:${input}`)
+  const translate = () => {
+    if (!$('#input-box')) return
+    let input = $('#input-box').text()
+    console.info(`input:${input}`)
     let output = marked(input)
     //console.info(`output:${output}`)
-    outputBox.innerHTML = output
-    // outputBox.querySelectorAll('pre code').forEach(block => {
-    //   hljs.highlightBlock(block)
-    // })
+    if ($('#output-box')) {
+      $('#output-box').html(output)
+    }
+  }
+
+  const highlightCode = () => {
+    $('#output-box') && $('#output-box p code').each((i, e) => {
+      hljs.highlightBlock(e)
+    })
+  }
+
+  if (window.innerWidth <= 767) {
+    $('#output-box') && $('#output-box').hide()
+  }
+
+  $(document).on('keyup', '#input-box', () => {
+    translate()
+    highlightCode()
   })
+
+  $(document).on('touchstart', '#code-btn', () => {
+    $('#input-box').show()
+    $('#output-box').hide()
+  })
+
+  $(document).on('touchstart', '#preview-btn', () => {
+    $('#input-box').hide()
+    $('#output-box').show()
+    translate()
+    highlightCode()
+  })
+
 
 })
 
